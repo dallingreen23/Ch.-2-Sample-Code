00594e2d81de4ecf48845c8248f4764cedda0ba6
Lab <Lab@AIS-204LAB22>
2017-01-18 17:13:54 -0700

    Add project files.

---
+++ PartyInvites_DallinGreen/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites_DallinGreen.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int HOUR = DateTime.Now.Hour;
+            ViewBag.Greeting = Hour < 12 ? "Good Morning" : "Good Afternoon"
+            return View();
+        }
+    }
+}
---
+++ PartyInvites_DallinGreen/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PartyInvites_DallinGreen.MvcApplication" Language="C#" %>
